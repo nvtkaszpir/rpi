87dc0eede11164d3be8ce55ad456179153dc3695
Michał Sochoń <kaszpir@gmail.com>
2019-11-18 22:52:21 +0100

    Fix script path

--- donkey/config.sh
+++ donkey/config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+
 export DEBIAN_FRONTEND=noninteractive
 
 
 # increase swap
 systemctl stop dphys-swapfile
-cp -f etc/dphys-swapfile /etc/dphys-swapfile
+cp -f "${SCRIPT_DIR}/etc/dphys-swapfile" /etc/dphys-swapfile
 systemctl start dphys-swapfile
--- wifi-ap/config-wifi-ap.sh
+++ wifi-ap/config-wifi-ap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 # set up Raspberry Pi as WiFi Access Point with NAT over eth0
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -19,18 +20,18 @@ systemctl enable chrony
 systemctl start chrony
 
 # enable NAT
-cp -f etc/sysctl.d/ipv4-forward.conf /etc/sysctl.d/ipv4-forward.conf
+cp -f "${SCRIPT_DIR}/etc/sysctl.d/ipv4-forward.conf" /etc/sysctl.d/ipv4-forward.conf
 iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables/iptables.ipv4.nat
-cp -f etc/rc.local /etc/rc.local
+cp -f "${SCRIPT_DIR}/etc/rc.local" /etc/rc.local
 
 # set static IP for WiFi card
-cp -f etc/dhcpcd.conf /etc/dhcpcd.conf
+cp -f "${SCRIPT_DIR}/etc/dhcpcd.conf" /etc/dhcpcd.conf
 
 # set up dnsmasq as DNS server and DHCP server
 systemctl enable dnsmasq
 systemctl stop dnsmasq
-cp -f etc/dnsmasq.conf /etc/dnsmasq.conf
+cp -f "${SCRIPT_DIR}/etc/dnsmasq.conf" /etc/dnsmasq.conf
 
 # disable current WiFi
 systemctl stop wpa_supplicant.service
@@ -40,7 +41,7 @@ systemctl disable wpa_supplicant.service
 systemctl unmask hostapd
 systemctl enable hostapd
 systemctl stop hostapd
-cp -f etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
-cp -f etc/default/hostapd /etc/default/hostapd
+cp -f "${SCRIPT_DIR}/etc/hostapd/hostapd.conf" /etc/hostapd/hostapd.conf
+cp -f "${SCRIPT_DIR}/etc/default/hostapd" /etc/default/hostapd
 
 echo "You should reboot now."
