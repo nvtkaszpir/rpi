c20b3d5acc4a4c992ea14f4e702a2ea473021c8c
Michał Sochoń <kaszpir@gmail.com>
2020-03-15 21:42:37 +0100

    Add docker install, default disabled

---
+++ base/config-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl
+
+echo 'deb [arch=armhf] https://download.docker.com/linux/raspbian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-get update
+
+# must use --no-install-recommends because aufs is in recommended and it fails
+# to install, yet it is not needed to run containers
+sudo apt install --no-install-recommends docker-ce
+sudo systemctl stop docker
+sudo systemctl disable docker
